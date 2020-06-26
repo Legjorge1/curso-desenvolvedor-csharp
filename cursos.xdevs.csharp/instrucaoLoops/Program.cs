3e622e9c2eb0d8a816e57b44e0211d7945d29984
Diogo Costa <diogotj@gmail.com>
2020-06-26 18:39:41 -0300

    completando os códigos para nossa primeira aula
    inserindo ifs, e switch cases

---
+++ cursos.xdevs.csharp/instrucaoLoops/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace instrucaoLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //loopComfor();
+            //numerosLoteria();
+            //loopComForeach();
+            //loopWhile();
+        }
+
+        static void loopComfor() 
+        {
+            // contar até 10 e incrementar de 2 em 2
+            for (int contador = 0; contador <= 10; contador += 2)
+            {
+                Console.WriteLine(contador);
+            }
+
+            // contar até 10 e incrementar de 1 em 1
+            for ( int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            //Vamos falar de incrementar
+            //++, --, +=, *=,
+        }
+
+        static void numerosLoteria() 
+        {
+            // variável que vai conter todos os números possíveis
+            int[] numerosLoteria = new int[59];
+            // variável utilizado para simular os número da loteria
+            int[] seusNumeros = new int[6];
+            // instanciamos um objeto para poder randomizar os números
+            Random rnd = new Random();
+            // populamos um range de 1 até 59 
+            for (int i = 0; i < 59; i++)
+            {
+                numerosLoteria[i] = i + 1;
+            }
+            // pegar 6 números randomicos
+            for (int selecionado = 0; selecionado < 6; selecionado++)
+            {
+                seusNumeros[selecionado] = numerosLoteria[rnd.Next(59)];
+            }
+
+            Console.WriteLine("Seus números da loteria são:");
+            for (int j = 0; j < 6; j++)
+            {
+                Console.Write(" " + seusNumeros[j] + " ");
+            }
+            Console.ReadLine();
+        }
+    
+        static void loopComForeach() 
+        {
+            int[] arrayDeInts = new int[] { 78, 89, 90, 76, 98, 65 };
+            foreach (int numeros in arrayDeInts)
+            {
+                Console.WriteLine(numeros);
+            }
+
+            string[] arrayDeString = new string[] { "item 1", "item 2", "item 3", "item 4", "item 5" };
+            foreach (string texto in arrayDeString)
+            {
+                Console.WriteLine(texto);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void loopWhile() 
+        {
+            int valor = 0;
+            while (valor < 10) //Enquanto a variável for menor que 10, execute esse bloco de código
+            {
+                Console.WriteLine(valor);
+                valor++; //se não incremetarmos a váriavel esse loop será eterno
+            }
+
+            Console.ReadLine();
+        }
+
+        static void loopDoWhile() 
+        {
+            int valor = 0;
+            do //--o bloco será executado inicialmente
+            {
+                Console.WriteLine(valor);
+                valor++; //--incremento
+            } while (valor < 10); //--validar se o bloco continuará a ser executado
+        }
+
+
+    }
+}
--- cursos.xdevs.csharp/intrucaoSwitch/Program.cs
+++ cursos.xdevs.csharp/intrucaoSwitch/Program.cs
@@ -10,7 +10,39 @@ namespace intrucaoSwitch
     {
         static void Main(string[] args)
         {
+            string condicao = "Olá";
+            switch (condicao)
+            {
+                case "Bom dia":
+                    Console.WriteLine("bom dia para você");
+                    break;
+                case "Olá":
+                    Console.WriteLine("Olá! tudo bem?");
+                    break;
+                case "Boa tarde":
+                    Console.WriteLine("boa");
+                    Console.WriteLine("tarde");
+                    break;
+                default:
+                    Console.WriteLine("boa noite");
+                    break;
+            }
 
+            int valor = 1;
+
+            switch (valor)
+            {
+                case 0:
+                case 1:
+                case 2:
+                    Console.WriteLine("caso 0, 1, 2.");
+                    break;
+                case -1:
+                case -10:
+                    Console.WriteLine("caso -1 -10.");
+                    break;
+                    //podemos omitir o default:
+            }
         }
     }
 }
