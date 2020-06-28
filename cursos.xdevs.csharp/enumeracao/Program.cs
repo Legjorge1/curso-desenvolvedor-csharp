ef6204d33a81ddcf44aba57fb650f6be0439f7da
Diogo Costa <diogotj@gmail.com>
2020-06-28 17:39:36 -0300

    adicionando codigo na aula 02
    Structs, Enums adicionados

---
+++ cursos.xdevs.csharp/enumeracao/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enumeracao
+{
+    class Program
+    {
+        //inicia no 0
+        //trabalha com inteiro
+        enum Meses1 { Jan, Feb, Mar, Apr, Mai, Jun, Jul, Ago, Set, Otu, Nov, Dec };
+        enum Meses2 { Jan = 1, Feb, Mar, Apr, Mai, Jun, Jul, Ago, Set, Otu, Nov, Dec };
+        enum Meses3 : byte { Jan = 1, Feb, Mar, Apr, Mai, Jun, Jul, Ago, Set, Otu, Nov, Dec };
+
+        static void Main(string[] args)
+        {
+            string nome = Enum.GetName(typeof(Meses2), 8);
+            Console.WriteLine("Oitavo mes nesse enum é: " + nome);
+            Console.WriteLine("veja os valores dos meses no enum:");
+            foreach (int values in Enum.GetValues(typeof(Meses2)))
+            {
+                Console.WriteLine(values);
+            }
+        }
+    }
+}
--- cursos.xdevs.csharp/explicandoStructs/Program.cs
+++ cursos.xdevs.csharp/explicandoStructs/Program.cs
@@ -10,6 +10,85 @@ namespace explicandoStructs
     {
         static void Main(string[] args)
         {
+            //struct é um tipo (value type), mas um tipo complexo
+
+            Estudante estudante = new Estudante();
+            estudante.primeiroNome = "Diogo";
+
+            Livro meuLivro = new Livro("Nome do Livro", "Programação", "Diogo Costa", 10, 1, 112345, "Capa dura");
+            //propriedades
+            Console.WriteLine(meuLivro.titulo);
+            Console.WriteLine(meuLivro.categoria);
+            Console.WriteLine(meuLivro.autor);
+            Console.WriteLine(meuLivro.numPaginas);
+            Console.WriteLine(meuLivro.paginaCorrente);
+            Console.WriteLine(meuLivro.ISBN);
+            Console.WriteLine(meuLivro.estiloCapa);
+
+            //ações
+            meuLivro.proximaPagina();
+            meuLivro.voltarPagina();
         }
     }
+
+    struct Estudante
+    {
+        public string primeiroNome { get; set; }
+        public string ultimoNome { get; set; }
+        public char inicial { get; set; }
+        public double media1 { get; set; }
+        public double media2 { get; set; }
+        public double media3 { get; set; }
+        public double media4 { get; set; }
+        public double mediaFinal { get; set; }
+    }
+
+    public struct Livro
+    {
+        public string titulo;
+        public string categoria;
+        public string autor;
+        public int numPaginas;
+        public int paginaCorrente;
+        public double ISBN;
+        public string estiloCapa;
+
+        //Construtor somente com todos as propriedades
+        public Livro(string titulo, string categoria, string autor, int numPaginas, int
+        paginaCorrente, double isbn, string estilocapa)
+        {
+            this.titulo = titulo;
+            this.categoria = categoria;
+            this.autor = autor;
+            this.numPaginas = numPaginas;
+            this.paginaCorrente = paginaCorrente;
+            this.ISBN = isbn;
+            this.estiloCapa = estilocapa;
+        }
+        public void proximaPagina()
+        {
+            if (paginaCorrente != numPaginas)
+            {
+                paginaCorrente++;
+                Console.WriteLine("A pagina corrente é: " + this.paginaCorrente);
+            }
+            else
+            {
+                Console.WriteLine("Final do livro.");
+            }
+        }
+        public void voltarPagina()
+        {
+            if (paginaCorrente != 1)
+            {
+                paginaCorrente--;
+                Console.WriteLine("Agora a página corrente é: " + this.paginaCorrente);
+            }
+            else
+            {
+                Console.WriteLine("Início do livro.");
+            }
+        }
+    }
+
 }
